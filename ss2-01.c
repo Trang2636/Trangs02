b3291b3ae3013e2c56ea78ce63ffeea5ed4ed7c3
Trang2636 <trangmaria123499@gmail.com>
2024-11-21 08:43:14 +0700

    Add files via upload

---
+++ ss2-01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	// so nguyen	
+	int integer;
+	// so thuc
+	float height;	
+	// ki tu
+	char a;
+	// so thuc , do chinh xac cao hon
+	double weight;
+	
+	return 0;
+	
+}
